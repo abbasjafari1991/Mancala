e36279de74ff8a4740cdaa8e25e80f868f9e312a
Abbas Jafari <abbas.jafari@gmail.com>
2023-10-17 18:15:13 +0200

    complete PlayerControllerTest

--- src/test/java/com/bol/mancala/rest/PlayerControllerTest.java
+++ src/test/java/com/bol/mancala/rest/PlayerControllerTest.java
@@ -1,46 +1,84 @@
 package com.bol.mancala.rest;
 
-import com.bol.mancala.service.PlayerService;
+import com.bol.mancala.repository.PlayerRepository;
 import com.bol.mancala.service.dto.PlayerDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@WebMvcTest(PlayerController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class PlayerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private PlayerService playerService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private PlayerRepository playerRepository;
 
     @Test
-    public void testCreatePlayer() throws Exception {
+    void testCreatePlayer() throws Exception {
         // Create a sample PlayerDTO
         PlayerDTO playerDTO = new PlayerDTO();
-        playerDTO.setId(1L);
         playerDTO.setName("Carol");
 
-        // Mock the service's save method
-        when(playerService.save(any(PlayerDTO.class))).thenReturn(playerDTO);
 
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/v1/player")
                         .contentType("application/json")
-                        .content("{\"name\":\"Carol\"}")
+                        .content(objectMapper.writeValueAsString(playerDTO))
                         .accept("application/json"))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Carol"));
     }
 
-    // Similar tests for other controller endpoints (GET, PUT, DELETE) can be added.
+    @Test
+    void testGetPlayerById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/v1/player/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Carol"));
+    }
+
+    @Test
+    void testUpdatePlayer() throws Exception {
+        // Create a sample PlayerDTO
+        PlayerDTO playerDTO = new PlayerDTO();
+        playerDTO.setId(1L);
+        playerDTO.setName("Bob");
+
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/v1/player/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(playerDTO))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Bob"));
+    }
+
+    @Test
+    void testDeletePlayer() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/v1/player/1"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+        assertEquals(playerRepository.count(), 0);
+    }
 }
